a7b5ba5da6c08e942333594304df0adeca99b13c
Boris <my@borissdjumajevs.webplace.lv>
2020-06-18 06:49:06 +0000

    Revert "migration class"
    This reverts commit e649c7e997cbb7de5f45a3bee942e3bba27a8f83

--- server/configs/config.js
+++ server/configs/config.js
@@ -1,9 +1,9 @@
 //Settings for DB connection
 var configs = {
     host: "localhost",
-    user: "back",
-    password: "Dbnzgblh321",
-    db: 'platform',
+    user: "MyUser",
+    password: "MyPass",
+    db: 'MyDb',
 };
 
 module.exports = configs;
